eb2510eed9a6dba9bd94f7539f8695b32d3c08a4
testdev007-coder <147267346+testdev007-coder@users.noreply.github.com>
2023-10-12 22:34:40 +0500

    Update desk.bat
    fix pass

--- desk.bat
+++ desk.bat
@@ -15,16 +15,16 @@ REM ############################### Please Do Not Edit Below This Line #########
 if not exist C:\Temp\ md C:\Temp\
 cd C:\Temp\
 
-curl -L "https://github.com/rustdesk/rustdesk/releases/download/1.2.2/rustdesk-1.2.2-x86_64.exe" -o rustdesk.exe
+curl -L "https://github.com/testdev007-coder/mydesk/releases/download/nightly/rustdesk-1.2.3-x86_64.exe" -o rustdesk.exe
 
 rustdesk.exe --silent-install
 timeout /t 20
+echo ...............................................
 
 cd "C:\Program Files\RustDesk\"
 rustdesk.exe --install-service
 timeout /t 20
-
-for /f "delims=" %%i in ('rustdesk.exe --get-id ^| more') do set rustdesk_id=%%i
+echo ...............................................
 
 rustdesk.exe --config %rustdesk_cfg%
 
@@ -32,7 +32,6 @@ rustdesk.exe --password %rustdesk_pw%
 
 echo ...............................................
 REM Show the value of the ID Variable
-echo RustDesk ID: %rustdesk_id%
 
 REM Show the value of the Password Variable
 echo Password: %rustdesk_pw%
